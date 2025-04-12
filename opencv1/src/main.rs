511a22c5c3847dc2a1db6d92b83034e49b1be213
poledna0 <henriquepoledna@proton.me>
2025-04-12 20:52:24 -0300

    criando opencv 2

--- opencv1/src/main.rs
+++ opencv1/src/main.rs
@@ -1,7 +1,8 @@
-use opencv;
+use opencv::{self, hdf::open};
 
 fn main() -> opencv::Result<()> {
     let imagem = opencv::imgcodecs::imread("imagem.png", opencv::imgcodecs::IMREAD_COLOR)?;
+
     opencv::highgui::imshow("Janela", &imagem)?;
     opencv::highgui::wait_key(0)?;
     Ok(())
