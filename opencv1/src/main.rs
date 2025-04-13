5b55f26cf3cac212fb9cd0366b00537898864379
poledna0 <henriquepoledna@proton.me>
2025-04-12 21:48:34 -0300

    nada q e do pobre funfa

--- opencv1/src/main.rs
+++ opencv1/src/main.rs
@@ -1,8 +1,7 @@
-use opencv::{self, hdf::open};
+use opencv;
 
 fn main() -> opencv::Result<()> {
     let imagem = opencv::imgcodecs::imread("imagem.png", opencv::imgcodecs::IMREAD_COLOR)?;
-
     opencv::highgui::imshow("Janela", &imagem)?;
     opencv::highgui::wait_key(0)?;
     Ok(())
