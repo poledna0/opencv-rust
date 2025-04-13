e4e45e15969f69655e695681e109f5caf9ddc900
poledna0 <henriquepoledna@proton.me>
2025-04-13 16:47:27 -0300

    add 1

--- duck/1.rs
+++ duck/1.rs
@@ -1,13 +1,13 @@
+//https://blog.devgenius.io/rust-and-opencv-bb0467bf35ff
 use anyhow::anyhow;
-use anyhow::Result;                 // | resenta uma imagem como uma matriz de 3 dimensões (altura, largura, canais RGB).
-use image::RgbImage;                // V
-use ndarray::{Array1, ArrayView1, ArrayView3}; //  ndarray serve para salva a img em formato rgb vale como um numpy da vida
+use anyhow::Result;
+use image::RgbImage;
+use ndarray::ArrayView3;
 use opencv::{self as cv, prelude::*};
 fn main() -> Result<()> {
-    // estamos lendo a imagem
-    let img = cv::imgcodecs::imread("./assets/img1.jpg", cv::imgcodecs::IMREAD_COLOR)?; // le a img em formato bgr o IMREAD_COLOR q faz ler nesse formato
-
-    // usa o algoritimo ORB para fazer os pontos
+    // Read image
+    let img = cv::imgcodecs::imread("./fotos/img1.jpeg", cv::imgcodecs::IMREAD_COLOR)?;
+    // Use Orb
     let mut orb = cv::features2d::ORB::create(
         500, // numero mx de keypoints
         1.2, // escala entre niveis de piramide
@@ -18,31 +18,27 @@ fn main() -> Result<()> {
         31, // patchSize
         20, // fastThreshold
     )?;
-    let mut orb_keypoints = cv::core::Vector::default(); // coordenadas dos pontos detectados.
-    let mut orb_desc = cv::core::Mat::default(); //descritores (representações numericas desses pontos).
-    let mut dst_img = cv::core::Mat::default(); //imagem onde vamos desenhar os pontos.
-    let mask = cv::core::Mat::default(); //define onde detectar pontos (aqui usamos toda a imagem).
-    orb.detect_and_compute(&img, &mask, &mut orb_keypoints, &mut orb_desc, false)?; // Detecção e cálculo dos descritores usando ORB.
-// desenha os keypoints na imagem com cor verde
-    cv::features2d::draw_keypoints( 
+    let mut orb_keypoints = cv::core::Vector::default();
+    let mut orb_desc = cv::core::Mat::default();
+    let mut dst_img = cv::core::Mat::default();
+    let mask = cv::core::Mat::default();
+    orb.detect_and_compute(&img, &mask, &mut orb_keypoints, &mut orb_desc, false)?;
+    cv::features2d::draw_keypoints(
         &img,
         &orb_keypoints,
         &mut dst_img,
         cv::core::VecN([0., 255., 0., 255.]),
         cv::features2d::DrawMatchesFlags::DEFAULT,
     )?;
-
-    // desenha um retângulo preenchido de cor azul no canto superior esquerdo da imagem
     cv::imgproc::rectangle(
         &mut dst_img,
         cv::core::Rect::from_points(cv::core::Point::new(0, 0), cv::core::Point::new(50, 50)),
-        cv::core::VecN([255., 0., 0., 0.]), // cor azul
-        -1, // preenchido
+        cv::core::VecN([255., 0., 0., 0.]),
+        -1,
         cv::imgproc::LINE_8,
         0,
     )?;
-
-    // msm coisa mas ag com o SIFT ele é mais robusto
+    // Use SIFT
     let mut sift = cv::features2d::SIFT::create(0, 3, 0.04, 10., 1.6)?;
     let mut sift_keypoints = cv::core::Vector::default();
     let mut sift_desc = cv::core::Mat::default();
@@ -51,11 +47,11 @@ fn main() -> Result<()> {
         &dst_img.clone(),
         &sift_keypoints,
         &mut dst_img,
-        cv::core::VecN([0., 0., 255., 255.]), // vermelho
+        cv::core::VecN([0., 0., 255., 255.]),
         cv::features2d::DrawMatchesFlags::DEFAULT,
     )?;
     // Write image using OpenCV
-    cv::imgcodecs::imwrite("./tmp.png", &dst_img, &cv::core::Vector::default())?; 
+    cv::imgcodecs::imwrite("./tmp.png", &dst_img, &cv::core::Vector::default())?;
     // Convert :: cv::core::Mat -> ndarray::ArrayView3
     let a = dst_img.try_as_array()?;
     // Convert :: ndarray::ArrayView3 -> RgbImage
@@ -67,7 +63,7 @@ fn main() -> Result<()> {
     // But since this is only a demo that
     // it indeed works to convert cv::core::Mat -> ndarray::ArrayView3
     // I'll let it be
-    test_image.save("out.png")?; //converte de ArrayView3 para RgbImage, que pode ser salva com a crate image
+    test_image.save("out.png")?;
 Ok(())
 }
 trait AsArray {
